ec7c98bf28f95b8be31dc4b8c1338f405086888e
Jakub Strnad <jakub.creative@gmail.com>
2023-03-31 14:18:50 +0200

    add mkdir in install.bat

--- install.bat
+++ install.bat
@@ -6,4 +6,6 @@ call venv\Scripts\activate.bat
 echo.
 echo. installing requirements...
 pip install -r requirements.txt
+mkdir _INPUT
+mkdir _OUTPUT
 echo Now you can run the run.bat to process all photos in _INPUT folder, you will find the cropped photos in _OUTPUT folder. Enjoy! :)
